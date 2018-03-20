26e1fa2d48d101ea38009747d96ba44360889915
emanueledemarchis <e.demarchis@emdm.it>
2018-03-20 15:37:00 +0100

    first

---
+++ poc/js/app/app.js
@@ -0,0 +1,28 @@
+﻿
+var myApp = {};
+var mainView = {};
+var rightView = {};
+var $$ = Dom7;
+
+var app = angular.module("AngularApp", []);
+app.run(function ($rootScope) {
+    myApp = new Framework7({
+        modalTitle: 'Framework7',
+        material: true,
+        pushState: true,
+        angular: true
+    });
+    mainView = myApp.addView('.view-main', {});
+    $rootScope.welcomescreen = _fnWelcomInit();
+})
+app.config(function () {
+    
+    window.location.hash = "#!/home.html";
+    
+})
+app.controller("RootController", ["$scope", function($scope) {
+  $scope.title = "Welcome Page";
+}])
+
+
+
---
+++ poc/js/app/controllers/home.ctr.js
@@ -0,0 +1,31 @@
+﻿app.controller("HomeController", ["$scope", "$http", '$rootScope', function ($scope, $http, $rootScope) {
+    $scope.title = "News Feeds";
+    $scope.nasa = [];
+    
+    $http({
+        "method": "GET",
+        "url": "http://rss2json.com/api.json?rss_url=https://www.nasa.gov/rss/dyn/breaking_news.rss"
+    }).then(function (data) {
+        $scope.nasa = data.data.items;
+    });
+
+    $http({
+        "method": "GET",
+        "url": "http://rss2json.com/api.json?rss_url=http://www.cnet.com/rss/smart-home/"
+    }).then(function (data) {
+        $scope.tech = data.data.items;
+    });
+
+    $http({
+        "method": "GET",
+        "url": "http://rss2json.com/api.json?rss_url=http://www.cnet.com/rss/gaming/"
+    }).then(function (data) {
+        $scope.games = data.data.items;
+        });
+
+    
+    $scope.fnOpen = function () {
+        $rootScope.welcomescreen.open();  
+    }
+    $scope.fnOpen();
+}]);

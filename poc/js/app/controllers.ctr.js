dbc9d8df3630d5027a7420efe1ea18e6eef8ff87
emanueledemarchis <e.demarchis@emdm.it>
2018-03-21 14:29:55 +0100

    fix

---
+++ poc/js/app/controllers.ctr.js
@@ -0,0 +1,12 @@
+﻿app.controller("HomeController", ["$scope", "$http", '$rootScope', 'userService', function ($scope, $http, $rootScope, $userService) {
+    $scope.user = $userService.getUser();
+    
+    $scope.openWelcom = function () {
+        $rootScope.welcomescreen.open();
+    }
+    //$scope.fnOpen();
+}]);
+app.controller("Step1Controller", ["$scope", "$http", '$rootScope', 'userService', function ($scope, $http, $rootScope, $userService) {
+    $scope.user = $userService.getUser();
+    
+}]);
--- poc/js/app/controllers/home.ctr.js
+++
@@ -1,8 +0,0 @@
-﻿app.controller("HomeController", ["$scope", "$http", '$rootScope', 'userService', function ($scope, $http, $rootScope, $userService) {
-    $scope.user = $userService.getUser();
-    
-    $scope.fnOpen = function () {
-       $rootScope.welcomescreen.open();  
-    }
-    //$scope.fnOpen();
-}]);

4ca6f9c53b079c96215c01cf3a6ef92324f18c81
emanueledemarchis <e.demarchis@emdm.it>
2018-03-29 11:34:33 +0200

    autocomplete

--- poc2/js/app/controllers.ctr.js
+++ poc2/js/app/controllers.ctr.js
@@ -12,6 +12,23 @@ app.controller("Step1Controller", ["$scope", "$http", '$rootScope', 'userService
     $scope.clear = function () {
         $scope.search = "";
     }
+    var autocompleteDropdownSimple = myApp.autocomplete({
+        input: '#autocomplete-dropdown',
+        openIn: 'dropdown',
+        source: function (autocomplete, query, render) {
+            var results = [];
+            if (query.length === 0) {
+                render(results);
+                return;
+            }
+            // Find matched items
+            for (var i = 0; i < $scope.users.length; i++) {
+                if ($scope.users[i].customerName.toLowerCase().indexOf(query.toLowerCase()) >= 0) results.push($scope.users[i].customerName);
+            }
+            // Render items by passing array with result items
+            render(results);
+        }
+    });
     $scope.filterFunction = function (element) {
         return element.customerName.toLowerCase().includes($scope.search.toLowerCase()) || $scope.search == "";
     };
